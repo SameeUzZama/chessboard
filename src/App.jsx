0076b57435afa057cbe349d51b8516ca3fbbdfa8
Samee Uz Zama <94825943+SameeUzZama@users.noreply.github.com>
2022-12-02 17:53:24 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { observe } from "./components/util/KnightObserver";
 import "./stylesheets/global.css";
 
 const App = () => {
-  // since all that is needed in this simple demo is a stream of values, entire app is wrapped by an observer that subscribes to a changing state in the most minimal, non-complex way (rather than using EventEmitter or making Game an object model)
+  // since all that is needed in this simple demo is a stream of values, entire app is wrapped by an observer that subscribes to a changing state in the most minimal, non-complex way (rather than using EventEmitter or making Game an object model).
   const [knightPosition, setKnightPosition] = useState([1, 7]);
   // the observe function will return an unsubscribe callback
   useEffect(() => observe((newPosition) => setKnightPosition(newPosition)));
