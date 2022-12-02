94b8621bce20c9da83c8cc57883f179e20e9b796
Samee Uz Zama <94825943+SameeUzZama@users.noreply.github.com>
2022-12-02 17:36:38 +0530

    Updates

---
+++ precache-manifest.879bbf705bae9334e1eedb39b5787e52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d67d4419553c2b9387fb",
+    "url": "/chessboard/static/css/main.97c7b43b.chunk.css"
+  },
+  {
+    "revision": "d67d4419553c2b9387fb",
+    "url": "/chessboard/static/js/main.8c9c34a9.chunk.js"
+  },
+  {
+    "revision": "0efd0562b0d04d42fa8a",
+    "url": "/chessboard/static/js/runtime~main.439ca143.js"
+  },
+  {
+    "revision": "7cc8c83c4060570c4470",
+    "url": "/chessboard/static/js/2.8ee7c57e.chunk.js"
+  },
+  {
+    "revision": "4cdb48d4f63e4ac922a3cc773a8268b5",
+    "url": "/chessboard/index.html"
+  }
+];
